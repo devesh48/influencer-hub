5613bd7fc628cca6e989903add0c3cfafae3e7c4
devesh48 <upadhyay.dev48@gmail.com>
2026-02-06 21:20:15 -0500

    updating code base

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,jsx}","./public/index.html"],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          600: '#7c3aed',
+          700: '#6d28d9',
+        },
+        accent: '#ec4899',
+      },
+    },
   },
   plugins: [],
 }
